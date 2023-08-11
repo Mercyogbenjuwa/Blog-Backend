95f501146ca2ff65ff57b59b788902a42747118d
Mercy Ogbenjuwa <99675525+Mercyogbenjuwa@users.noreply.github.com>
2023-08-11 11:49:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
+const PORT = process.env.PORT || 3001
 const app = express();
 const routes = require('./src/Routes/routes');
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
+
