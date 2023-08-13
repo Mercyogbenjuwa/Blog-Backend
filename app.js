4f39111a2519221dfdc0ab3fc291c00068e7b6f0
Mercy Ogbenjuwa <99675525+Mercyogbenjuwa@users.noreply.github.com>
2023-08-13 16:52:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const routes = require('./src/Routes/routes');
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', routes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
